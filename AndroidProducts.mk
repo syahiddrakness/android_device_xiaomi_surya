c0c9b479db600be8b9434d772d7e83b799606fcf
syahiddraakness <syahidselaluspd708@gmail.com>
2024-03-28 01:09:36 +0700

    surya: bringup aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020-2023 The LineageOS Project
+# Copyright (C) 2020-2023 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/aosp_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    aosp_surya-user \
+    aosp_surya-userdebug \
+    aosp_surya-eng
--- aosp_surya.mk
+++ aosp_surya.mk
@@ -12,22 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common  Project Matrixx stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-#Project Matrixx stuff
-MATRIXX_BUILD_TYPE := unofficial
-MATRIXX_CHIPSET := Qualcomm-sm8250
-MATRIXX_BATTERY := 10000mah
-MATRIXX_DISPLAY := 1080x2400
-MATRIXX_MAINTAINER := syahiddrakness
-TARGET_BOOT_ANIMATION_RES := 1080
-WITH_GMS := true
 
